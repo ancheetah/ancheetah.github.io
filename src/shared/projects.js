6d2629f0c89772d6e2352a10d5914d64791ca854
AJ <ancheetah@users.noreply.github.com>
2021-04-28 17:21:14 -0400

    projects content

---
+++ src/shared/projects.js
@@ -0,0 +1,92 @@
+export const PROJECTS = [
+    {
+        id: 0,
+        name: "On Deck",
+        img: "",
+        desc: "An e-commerce website for popular streetwear brands",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 1,
+        name: "4 Foodies",
+        img: "",
+        desc: "A static website design for a food blog",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 2,
+        name: "Portfolio",
+        img: "",
+        desc: "This website was built with React.js and reactstrap",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 3,
+        name: "To Do",
+        img: "",
+        desc: "A simple to-do list app",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 4,
+        name: "Gilligan Contracting",
+        img: "",
+        desc: "A website created with Squarespace for a small business",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 5,
+        name: "MADIFF",
+        img: "",
+        desc: "A website created with a Wordpress theme for a non-profit organization",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 6,
+        name: "Blossom + Birch",
+        img: "",
+        desc: "A website created with Squarespace for a cake design business",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 7,
+        name: "Codepen",
+        img: "",
+        desc: "Where I sometimes test out new features and small components",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    },
+    {
+        id: 8,
+        name: "Photography",
+        img: "",
+        desc: "",
+        demo: "",
+        repo: "",
+        tags: [],
+        features: []
+    }
+]

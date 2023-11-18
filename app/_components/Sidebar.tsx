88a1f77cc4fa6e67e3ee6a5d34f7e6f2cca425c9
AJ <ancheetah@users.noreply.github.com>
2023-11-18 18:39:42 -0500

    sidebar component

---
+++ app/_components/Sidebar.tsx
@@ -0,0 +1,23 @@
+export function Sidebar() {
+  return (
+    <aside className="fixed left-0 top-0 h-screen w-64">
+      <div className="flex h-full flex-col overflow-y-auto px-6 py-6">
+        <div className="mt-10">
+          <h1 className="text-4xl">AJ</h1>
+          <h1 className="text-4xl">Ancheta</h1>
+          <p className="mb-8 mt-8">
+            I&apos;m a <span>Front-End Developer</span> with a background in Astrophysics and passion for art.
+          </p>
+        </div>
+        <div className="flex h-full grow flex-col justify-center">
+          <ul className="text-2xl">
+            <li className="my-2">Tools</li>
+            <li className="my-2">Projects</li>
+            <li className="my-2">About</li>
+            <li className="my-2">Education</li>
+          </ul>
+        </div>
+      </div>
+    </aside>
+  )
+}
